76f5886bb18198e14c4b8f968575e61f432747d3
leonace924 <meobius123@gmail.com>
2021-11-01 05:54:40 -0400

    chore: update jsconfig, layout and characters

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" async defer />
           <link
-            href="https://fonts.googleapis.com/css2?family=Asap+Condensed:wght@400;500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Asap+Condensed:wght@400;500;600;700&amp;display=swap"
             rel="stylesheet"
           />
         </Head>
