acb9b132524d032fa5dd4b70d215aee2c2750097
leonace924 <meobius123@gmail.com>
2021-11-02 06:09:35 -0400

    fix: update fullpage menu location

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import FullPageMenu from '@components/FullPageMenu/FullPageMenu';
 import Footer from '@components/Footer/Footer';
 
 const Layout = ({ children, title = 'Great State Burger' }) => {
@@ -10,6 +11,7 @@ const Layout = ({ children, title = 'Great State Burger' }) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="Description" content="Great State Burger Site" />
       </Head>
+      <FullPageMenu />
 
       <main>{children}</main>
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import Layout from '@components/Layout/Layout';
-import FullPageMenu from '@components/FullPageMenu/FullPageMenu';
 import Hero from 'src/sections/Home/Hero/Hero';
 
 export default function Home() {
   return (
     <Layout>
-      <FullPageMenu />
       <Hero />
     </Layout>
   );
