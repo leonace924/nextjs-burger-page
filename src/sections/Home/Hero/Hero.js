eeaf0eb01a6edeb354a757a7ec0d414e4a501bd0
leonace924 <meobius123@gmail.com>
2021-11-02 04:06:50 -0400

    feat: add hero section

---
+++ src/sections/Home/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '@components/Header/Header';
+
+const Hero = () => {
+  return (
+    <div className="p-[22px] lg:p-8 bg-secondary">
+      <div className="bg-white">
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
