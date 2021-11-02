b0393405dd1d5d686022b54079a1a2d69d919954
leonace924 <meobius123@gmail.com>
2021-11-02 05:07:52 -0400

    feat: add carousel to project

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+import SwiperCore, { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { imagesList } from './constant';
+import 'swiper/css';
+
+const Carousel = () => {
+  SwiperCore.use([Navigation]);
+
+  return (
+    <div>
+      <Swiper
+        className="px-4"
+        spaceBetween={14}
+        slidesPerView={'auto'}
+        centeredSlides={true}
+        navigation={true}
+        breakpoints={{
+          768: {
+            spaceBetween: 32
+          },
+          1024: {
+            centeredSlides: false
+          }
+        }}
+      >
+        {imagesList?.map((image, id) => (
+          <SwiperSlide
+            key={id}
+            className="max-w-[250px] max-h-[250px] md:max-w-[300px] md:max-h-[300px]"
+          >
+            <div className="w-[250px] h-[250px] relative md:w-[300px] md:h-[300px] rounded-[10px] overflow-hidden">
+              <Image
+                src={image.imageUrl}
+                alt={`image-${id}`}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/components/Carousel/constant.js
@@ -0,0 +1,23 @@
+export const imagesList = [
+  {
+    imageUrl: '/assets/images/img-slide-01.jpg'
+  },
+  {
+    imageUrl: '/assets/images/img-slide-02.jpg'
+  },
+  {
+    imageUrl: '/assets/images/img-slide-03.jpg'
+  },
+  {
+    imageUrl: '/assets/images/img-slide-04.jpg'
+  },
+  {
+    imageUrl: '/assets/images/img-slide-05.jpg'
+  },
+  {
+    imageUrl: '/assets/images/img-slide-06.jpg'
+  },
+  {
+    imageUrl: '/assets/images/img-slide-07.jpg'
+  }
+];
